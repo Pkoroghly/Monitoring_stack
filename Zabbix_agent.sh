5ce4066b7b0733248f432a985b923c8c1f759a99
Pasha koroghly <79890366+Pkoroghly@users.noreply.github.com>
2023-08-20 21:14:44 +0330

    Update Zabbix_agent.sh

--- Zabbix_agent.sh
+++ Zabbix_agent.sh
@@ -4,6 +4,8 @@ wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix
 dpkg -i zabbix-release_6.4-1+ubuntu22.04_all.deb
 apt update
 apt install zabbix-agent
+systemctl start zabbix-agent
+systemctl enable zabbix-agent
 
 
 
